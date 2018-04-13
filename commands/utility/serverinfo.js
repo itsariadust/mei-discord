ade16d0c97bf10058e115584ac974433646b0635
Jakob Aindreas A. Rivera <jakobaindreas11@gmail.com>
2018-04-13 14:16:01 +0000

    Updated userinfo command, getting Date when a user mentioned is still not working. Added server info, still under construction

---
+++ commands/utility/serverinfo.js
@@ -0,0 +1,26 @@
+const Commando = require('discord.js-commando');
+const RichEmbed = require('discord.js');
+
+module.exports = class ServerInfoCommand extends Commando.Command {
+    
+    constructor(client) {
+        
+        super(client, {
+            
+            name: 'serverinfo',
+            group: 'utility',
+            memberName: 'serverinfo',
+            description: 'Displays the server info',
+            
+        });
+        
+    }
+    
+    run(msg) {
+        
+        const embed = new RichEmbed();
+        embed.setTitle('Guild Information');
+        
+    }
+    
+};
--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -11,10 +11,19 @@ module.exports = class UserInfoCommand extends Commando.Command {
     
     constructor(client) {
         super(client, {
-            name:'user',
+            name:'userinfo',
             group:'utility',
             memberName:'userinfo',
             description:'Displays user info. Much different from profile'
+            examples:['userinfo @Eris#6753','userinfo'],
+            args: [
+				{
+					key: 'member',
+					label: 'user',
+					prompt: 'What member should I get info?',
+					type: 'member'
+				}
+			]
         });
     }
     
@@ -28,16 +37,24 @@ module.exports = class UserInfoCommand extends Commando.Command {
             
             embed.setTitle('Information About' + " " + message.author.username);
             embed.setColor(color);
+            embed.addField("ID:", message.member.id);
+            embed.addField("Status", message.member.presence);
             embed.addField("Joined in Discord:", message.author.createdAt, true);
             embed.addField("Joined in server:", message.member.joinedAt, true);
+            embed.addField("Server Nickname:", message.member.nickname !== null ? `Nickname: ${message.member.nickname}` : 'No nickname set', true);
+            embed.addField("Server Roles:", message.member.roles.map(roles => `${roles.name}`).join(', '));
             return message.embed(embed).then(callback);
             
         } else
         
         embed.setTitle('Information About' + " " + member.displayName);
         embed.setColor(color);
-        embed.addField("Joined in Discord:", member.joinedAt, true);
-        embed.addField("Joined in server:", member.joinedTimestamp, true);
+        embed.addField("Status", member.presence);
+        embed.addField("ID:", member.id);
+        embed.addField("Joined in Discord:", "[unavailable]", true);
+        embed.addField("Joined in server:", member.joinedAt, true);
+        embed.addField("Server Nickname:", member.nickname !== null ? `Nickname: ${member.nickname}` : 'No nickname set', true);
+        embed.addField("Server Roles:", member.roles.map(roles => `${roles.name}`).join(', '));
         return message.embed(embed).then(callback);
         
     }
