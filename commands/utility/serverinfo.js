577c2dc2de18c2fc96630aa96e584972f0481ccc
itsYuuki <jakobaindreas11@gmail.com>
2018-04-14 19:10:46 +0800

    added serverinfo command

--- commands/utility/serverinfo.js
+++ commands/utility/serverinfo.js
@@ -1,5 +1,5 @@
 const Commando = require("discord.js-commando");
-const RichEmbed = require("discord.js");
+const { RichEmbed } = require("discord.js");
 
 module.exports = class ServerInfoCommand extends Commando.Command {
 
@@ -16,10 +16,46 @@ module.exports = class ServerInfoCommand extends Commando.Command {
 
   }
 
-  run() {
+  run(message, callback) {
 
+    let vLevelNum = message.guild.verificationLevel;
+    let vLevel;
+
+    if (vLevelNum === 0) {
+
+      vLevel = "None";
+
+    } else if (vLevelNum === 1) {
+
+      vLevel = "Low";
+
+    } else if (vLevelNum === 2) {
+
+      vLevel = "Medium";
+
+    } else if (vLevelNum === 3) {
+
+      vLevel = "High";
+
+    } else if (vLevelNum === 4) {
+
+      vLevel = "Extreme";
+
+    }
+
+    let owner = message.guild.owner;
     const embed = new RichEmbed();
-    embed.setTitle("Guild Information");
+    embed.setAuthor(message.guild.name, message.guild.iconURL);
+    embed.addField("ID", message.guild.id);
+    embed.addField("Server Owner", owner.user.username + "#" + owner.user.discriminator);
+    embed.addField("Verification Level", vLevel);
+    embed.addField("Region", message.guild.region);
+    embed.addField("Channels", message.guild.channels.size, true);
+    embed.addField("Members", message.guild.memberCount, true);
+    embed.addField("Created On", message.guild.createdAt);
+    embed.addField("Roles", message.guild.roles.size);
+
+    return message.embed(embed).then(callback);
 
   }
 
