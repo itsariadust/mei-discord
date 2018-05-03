7cdcfd55cb66837d9ed039c3291ed0293e15a98a
itsYuuki <jakobaindreas11@gmail.com>
2018-05-03 14:58:51 +0800

    Updated warn DM message

--- commands/mod/warn.js
+++ commands/mod/warn.js
@@ -1,30 +1,37 @@
 const Commando = require("discord.js-commando");
+const stripIndents = require("common-tags").stripIndents;
 
 module.exports = class WarnCommand extends Commando.Command {
   constructor(client) {
-    super (client,{
-      name:"warn",
-      group:"mod",
-      memberName:"warn",
-      description:"Warns a member",
-      userPermissions:["MANAGE_GUILD","ADMINISTRATOR"],
-      args:[
-        {
-          key:"member",
-          prompt:"Please mention a member to warn",
-          type:"member"
-        },
-        {
-          key:"warningMsg",
-          prompt:"Please enter a warning message.",
-          type:"string"
-        }
+    super(client, {
+      name: "warn",
+      group: "mod",
+      memberName: "warn",
+      description: "Warns a member",
+      userPermissions: ["MANAGE_GUILD", "ADMINISTRATOR"],
+      args: [{
+        key: "member",
+        prompt: "Please mention a member to warn",
+        type: "member"
+      },
+      {
+        key: "warningMsg",
+        prompt: "Please enter a warning message.",
+        type: "string"
+      }
       ]
     });
   }
 
-  run (message, { member, warningMsg }) {
-    member.send("You have been warned for" + " " + `${warningMsg}`);
+  run(message, {
+    member,
+    warningMsg
+  }) {
+    member.send(stripIndents`
+    You have been warned in the server: ${message.guild.name}!
+    By: "${message.author.tag}" 
+    "Reason: "${warningMsg}"
+    `);
     return message.say("Done");
   }
 };
