32245606d1232b73550ecd0109c261432707c6d6
itsYuuki <jakobaindreas11@gmail.com>
2018-05-04 19:33:38 +0800

    Cosmetic changes...again

--- commands/mod/modrole.js
+++ commands/mod/modrole.js
@@ -4,27 +4,26 @@ const fs = require("fs");
 
 module.exports = class ModRoleCommand extends Commando.Command {
   constructor(client) {
-    super(client,{
-      name:"modrole",
-      group:"mod",
-      memberName:"modrole",
-      description:"Adds/Removes a role for mod commands. (Can only be used by the server owner)",
-      args:[
-        {
-          key:"action",
-          prompt:"What action would you like to preform? `add`, `remove`, or `list`",
-          type:"string"
-        },
-        {
-          key:"role",
-          prompt:"Please enter a role name. (Make sure the role is mentionable for this command to work)",
-          type:"role"
-        }
+    super(client, {
+      name: "modrole",
+      group: "mod",
+      memberName: "modrole",
+      description: "Adds/Removes a role for mod commands. (Can only be used by the server owner)",
+      args: [{
+        key: "action",
+        prompt: "What action would you like to preform? `add`, `remove`, or `list`",
+        type: "string"
+      },
+      {
+        key: "role",
+        prompt: "Please enter a role name. (Make sure the role is mentionable for this command to work)",
+        type: "role"
+      }
       ]
     });
   }
 
-  run (message, args) {
+  run(message, args) {
     // adds a mod role
     if (args.action.toLowerCase() === "add") {
       if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES", false, true, true)) {
@@ -38,8 +37,7 @@ module.exports = class ModRoleCommand extends Commando.Command {
       let id = args.role.id;
       if (modRole[message.guild.id].modroles.includes(id)) {
         return message.reply("The mod role has already been added.");
-      }
-      else {
+      } else {
         modRole[message.guild.id].modroles.push(id);
         fs.writeFile("./assets/json/modrole.json", JSON.stringify(modRole, null, 2), (err) => {
           if (err) {
