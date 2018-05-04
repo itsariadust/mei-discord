5af43f8f9e8200c8d4f8ee5feba3143f6a2dae77
itsYuuki <jakobaindreas11@gmail.com>
2018-05-04 22:23:34 +0800

    Linters suck

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -3,18 +3,18 @@ const commando = require("discord.js-commando");
 class SayCommand extends commando.Command {
   constructor(client) {
     super(client, {
-      name: 'say',
-      group: 'fun',
-      memberName: 'say',
-      description: 'Sends a message through ara, then deletes the previous message sent.',
+      name: "say",
+      group: "fun",
+      memberName: "say",
+      description: "Make Mei say anything",
       args: [{
-        key: 'text',
-        prompt: 'What do you want me to say?',
-        type: 'string'
+        key: "text",
+        prompt: "What do you want me to say?",
+        type: "string"
       }]
     });
   }
-  async run(message, args) {
+  run(message, args) {
     if (message.mentions.channels.first()) {
       message.delete().catch(console.error);
       message.mentions.channels.first().send(args.text.split(" ").slice(1).join(" "));
