7d58d819f647a2746104d2853f7b3d0fdee6de1f
itsYuuki <jakobaindreas11@gmail.com>
2018-05-04 20:09:25 +0800

    Updated say command

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -3,29 +3,26 @@ const Commando = require("discord.js-commando");
 module.exports = class SayCommand extends Commando.Command {
 
   constructor(client) {
-
     super(client, {
       name: "say",
       group: "fun",
       memberName: "say",
       description: "make the bot say something",
-      args: [
-        {
-          key: "text",
-          prompt: "Make the bot say something",
-          type: "string",
-        },
-      ],
-
+      args: [{
+        key: "text",
+        prompt: "Make the bot say something",
+        type: "string",
+      }],
     });
-
   }
 
-  run(msg, args) {
-    const { text } = args;
-    msg.delete();
-    return msg.say(`${text}`);
-
+  run(message, args) {
+    if (message.mentions.channels.first()) {
+      message.delete().catch(console.error);
+      message.mentions.channels.first().send(args.text.split(" ").slice(1).join(" "));
+    } else {
+      message.send(args.text);
+      message.delete().catch(console.error);
+    }
   }
-
 };
