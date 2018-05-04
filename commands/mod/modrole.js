b083c30e0b1ff2fa609559882785f6a041b321bc
itsYuuki <jakobaindreas11@gmail.com>
2018-05-04 21:11:04 +0800

    Fixed permissions

--- commands/mod/modrole.js
+++ commands/mod/modrole.js
@@ -24,49 +24,51 @@ module.exports = class ModRoleCommand extends Commando.Command {
   }
 
   run(message, args) {
-    // adds a mod role
-    if (args.action.toLowerCase() === "add") {
-      if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES", false, true, true)) {
-        return message.reply("You do not have permission to execute this Command");
+    if (message.author.id === message.guild.OwnerID) {
+      // adds a mod role
+      if (args.action.toLowerCase() === "add") {
+        if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES", false, true, true)) {
+          return message.reply("You do not have permission to execute this Command");
+        }
+        if (!modRole[message.guild.id]) {
+          modRole[message.guild.id] = {
+            modroles: []
+          };
+        }
+        let id = args.role.id;
+        if (modRole[message.guild.id].modroles.includes(id)) {
+          return message.reply("The mod role has already been added.");
+        } else {
+          modRole[message.guild.id].modroles.push(id);
+          fs.writeFile("./assets/json/modrole.json", JSON.stringify(modRole, null, 2), (err) => {
+            if (err) {
+              message.reply("Something went wrong! Contact Eris#6753");
+              return console.log(err);
+            }
+            return message.reply("Mod role added.");
+          });
+        }
       }
-      if (!modRole[message.guild.id]) {
-        modRole[message.guild.id] = {
-          modroles: []
-        };
-      }
-      let id = args.role.id;
-      if (modRole[message.guild.id].modroles.includes(id)) {
-        return message.reply("The mod role has already been added.");
-      } else {
-        modRole[message.guild.id].modroles.push(id);
+
+      // removes a mod role
+      if (args.action.toLowerCase() === "remove") {
+        let roleIndex = modRole[message.guild.id].modroles.indexOf(args.role);
+
+        if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES", false, true, true)) {
+          return message.reply("You don't have permissions to execute this command.");
+        }
+        if (roleIndex === null) {
+          return message.reply("Role not found. Perhaps you misspelled it or you didn't list it in");
+        }
+        modRole[message.guild.id].modroles.splice(roleIndex, 1);
         fs.writeFile("./assets/json/modrole.json", JSON.stringify(modRole, null, 2), (err) => {
           if (err) {
             message.reply("Something went wrong! Contact Eris#6753");
             return console.log(err);
           }
-          return message.reply("Mod role added.");
+          return message.reply("Mod role removed.");
         });
       }
-    }
-
-    // removes a mod role
-    if (args.action.toLowerCase() === "remove") {
-      let roleIndex = modRole[message.guild.id].modroles.indexOf(args.role);
-
-      if (!message.guild.member(message.author).hasPermission("MANAGE_ROLES", false, true, true)) {
-        return message.reply("You don't have permissions to execute this command.");
-      }
-      if (roleIndex === null) {
-        return message.reply("Role not found. Perhaps you misspelled it or you didn't list it in");
-      }
-      modRole[message.guild.id].modroles.splice(roleIndex, 1);
-      fs.writeFile("./assets/json/modrole.json", JSON.stringify(modRole, null, 2), (err) => {
-        if (err) {
-          message.reply("Something went wrong! Contact Eris#6753");
-          return console.log(err);
-        }
-        return message.reply("Mod role removed.");
-      });
     }
   }
 };
