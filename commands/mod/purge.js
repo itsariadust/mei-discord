a6b5307079c7efcf93b655aca0b8aa4ed13f3399
itsYuuki <jakobaindreas11@gmail.com>
2018-05-05 16:37:41 +0800

    Added purge command

---
+++ commands/mod/purge.js
@@ -0,0 +1,28 @@
+const Commando = require("discord.js-commando");
+const modRole = require("../../assets/json/modrole.json");
+
+module.exports = class PurgeCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "purge",
+      group: "mod",
+      memberName: "purge",
+      description: "Purges an amount of message in a channel (Max is 100)",
+      clientPermissions: ["MANAGE_MESSAGES"],
+      args: [{
+        key:"number",
+        prompt:"How many message do you want to delete?",
+        type:"number"
+      }]
+    });
+  }
+
+  run(message, args) {
+    if (message.member.roles.some(r => modRole[message.guild.id].modroles.includes(r.id) || message.author.id === message.guild.ownerID)) {
+      message.channel.bulkDelete(args.number);
+      return message.delete();
+    } else {
+      return message.reply("You don't have the permission to execute this command");
+    }
+  }
+};
