5b9672b80d35e9eb65e33c7099895e04e03ab87f
itsYuuki <jakobaindreas11@gmail.com>
2018-05-06 19:56:55 +0800

    New lick command and some updates

--- commands/roleplay/lick.js
+++ commands/roleplay/lick.js
@@ -1,39 +1,30 @@
 const Commando = require("discord.js-commando");
-const { RichEmbed } = require("discord.js");
+const {
+  RichEmbed
+} = require("discord.js");
 
-module.exports = class kissCommand extends Commando.Command {
-
+module.exports = class LickCOmmand extends Commando.Command {
   constructor(client) {
-
     super(client, {
-
       name: "kiss",
       group: "roleplay",
       memberName: "kiss",
       description: "*kisses*",
-
     });
-
   }
 
   run(message) {
-
-    const kissIndex = ["https://media1.tenor.com/images/78095c007974aceb72b91aeb7ee54a71/tenor.gif?itemid=5095865", "https://media1.tenor.com/images/1306732d3351afe642c9a7f6d46f548e/tenor.gif?itemid=6155670", "https://media1.tenor.com/images/7fd98defeb5fd901afe6ace0dffce96e/tenor.gif?itemid=9670722", "https://media1.tenor.com/images/621ceac89636fc46ecaf81824f9fee0e/tenor.gif?itemid=4958649", "https://media1.tenor.com/images/bc5e143ab33084961904240f431ca0b1/tenor.gif?itemid=9838409"];
+    const kissIndex = ["https://media1.tenor.com/images/5f73f2a7b302a3800b3613095f8a5c40/tenor.gif?itemid=10005495", "https://media1.tenor.com/images/d702fa41028207c6523b831ec2db9467/tenor.gif?itemid=5990650", "https://media1.tenor.com/images/5785566574fe6293e3be673e85d4894b/tenor.gif?itemid=5649365", "https://media1.tenor.com/images/feeef4685f9307b76c78a22ba0a69f48/tenor.gif?itemid=8413059", "https://media1.tenor.com/images/df6d1b4922c131f1d191f022a3dbaf67/tenor.gif?itemid=11357830"];
     const member = message.mentions.members.first();
     const guildMembers = message.guild.members.map(gm => gm.id);
     const randMember = guildMembers[Math.floor(Math.random() * guildMembers.length)];
-
     if (!member) {
       message.channel.send(message.author.username + " kisses " + "<@" + randMember + ">");
-    }
-    else {
+    } else {
       message.channel.send(message.author.username + " kisses " + `${member}`);
     }
-
     const embed = new RichEmbed()
       .setImage(kissIndex[Math.floor(Math.random() * kissIndex.length)]);
     message.channel.send({embed});
-
   }
-
 };
