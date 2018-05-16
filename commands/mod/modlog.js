b5dec83e01137db380cd8788ef7a1dafaebd24ed
itsYuuki <jakobaindreas11@gmail.com>
2018-05-16 12:26:04 +0800

    Added permissions

--- commands/mod/modlog.js
+++ commands/mod/modlog.js
@@ -1,4 +1,5 @@
 const Commando = require("discord.js-commando");
+const modRole = require("../../assets/json/modrole.json");
 
 module.exports = class ModLogSetCommand extends Commando.Command {
   constructor(client) {
@@ -16,18 +17,24 @@ module.exports = class ModLogSetCommand extends Commando.Command {
   }
 
   run(message, args) {
-    if (args.action.toLowerCase() === "add") {
-      let channel = message.mentions.channels.first();
-      if (!channel) return message.reply("Please specify a channel");
+    if (!modRole[message.guild.id]) return message.reply("There are no roles set up for this comamnd to run");
+    
+    if (message.member.roles.some(r => modRole[message.guild.id].modroles.includes(r.id)) || message.author.id === message.guild.ownerID) {
+      if (args.action.toLowerCase() === "add") {
+        let channel = message.mentions.channels.first();
+        if (!channel) return message.reply("Please specify a channel");
 
-      let channelID = channel.id;
-      message.guild.settings.set("modlog", channelID);
-      return message.reply(`Done. Set the mod log channel to "<#${message.guild.settings.get("modlog")}>"`);
-    }
+        let channelID = channel.id;
+        message.guild.settings.set("modlog", channelID);
+        return message.reply(`Done. Set the mod log channel to "<#${message.guild.settings.get("modlog")}>"`);
+      }
 
-    if (args.action.toLowerCase() === "remove") {
-      message.guild.settings.remove("modlog");
-      return message.reply(`Done. Removed the modlog channel. Do \`${message.guild.commandPrefix}settings add modlog\` to set a new channel for modlogging`);
+      if (args.action.toLowerCase() === "remove") {
+        message.guild.settings.remove("modlog");
+        return message.reply(`Done. Removed the modlog channel. Do \`${message.guild.commandPrefix}settings add modlog\` to set a new channel for modlogging`);
+      }
+    } else {
+      return message.reply("You don't have the permissions to run this command. Do you have the roles for the moderator commands? Please contact the server oewner to fix this");
     }
   }
 };
