19b2fc67a5a75b61b33bcc603d0d8d127e3775ce
itsYuuki <2149694+itsYuuki@users.noreply.github.com>
2018-05-19 17:29:53 +0800

    Update about.js

--- commands/core/about.js
+++ commands/core/about.js
@@ -20,7 +20,7 @@ module.exports = class AboutCommand extends Commando.Command {
     embed.setColor(0x551A8B);
     embed.setDescription("Mei is a discord bot made by Eris#6753 using discord.js. Commands handled by discord.js-commando. For help please type `m!help`");
     embed.addField("Owner","Eris#6753");
-    embed.addField("Version","v1.1.2");
+    embed.addField("Version","v1.1.3");
     return msg.embed(embed).then(callback);
 
   }
