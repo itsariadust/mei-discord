81fb02191bdfaa27f53abd42d9656eb2e1af6585
itsYuuki <jakobaindreas11@gmail.com>
2018-05-22 10:08:30 +0800

    removed unused 'args' parameter

--- commands/reactions/smug.js
+++ commands/reactions/smug.js
@@ -1,19 +1,19 @@
-const commando = require("discord.js-commando")
+const commando = require("discord.js-commando");
 
 class SmugCommand extends commando.Command {
-    constructor(client) {
-        super(client, {
-            name: "smug",
-            group: "reactions",
-            memberName: "smug",
-            description: "Smug"
-        });
-    }
-    async run(message, args) {
-        message.delete().catch(console.error);
-        var imgNo = Math.floor(Math.random() * 58) + 1;
-        message.channel.send("http://smug.moe/smg/" + imgNo + ".png")
-    }
+  constructor(client) {
+    super(client, {
+      name: "smug",
+      group: "reactions",
+      memberName: "smug",
+      description: "Smug"
+    });
+  }
+  run(message) {
+    message.delete().catch(console.error);
+    var imgNo = Math.floor(Math.random() * 58) + 1;
+    message.channel.send("http://smug.moe/smg/" + imgNo + ".png");
+  }
 }
 
 module.exports = SmugCommand;
