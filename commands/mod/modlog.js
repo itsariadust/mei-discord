04e2ee94266901c3d78e6b465e84de8023d69358
itsYuuki <jakobaindreas11@gmail.com>
2018-07-04 15:24:46 +0800

    added second optional argument

--- commands/mod/modlog.js
+++ commands/mod/modlog.js
@@ -20,7 +20,7 @@ module.exports = class ModLogSetCommand extends Commando.Command {
     if (!modRole[message.guild.id]) return message.reply("There are no roles set up for this comamnd to run");
     
     if (message.member.roles.some(r => modRole[message.guild.id].modroles.includes(r.id)) || message.author.id === message.guild.ownerID) {
-      if (args.action.toLowerCase() === "add") {
+      if (args.action.toLowerCase() === "add" || "set") {
         let channel = message.mentions.channels.first();
         if (!channel) return message.reply("Please specify a channel");
 
