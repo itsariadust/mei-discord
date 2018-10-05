6c0da20a7a7821a6d0571123d4bf3ba5907ce7f1
itsYuuki <jakobaindreas11@gmail.com>
2018-10-05 13:24:44 +0800

    Added ready event

---
+++ events/ready.js
@@ -0,0 +1,17 @@
+const chalk = require("chalk");
+
+module.exports = client => {
+  console.log("Starting up Mei...");
+
+  console.log(chalk.green(`Mei is up and running.\nOperating on ${client.guilds.size} servers.\nOperating for ${client.users.size} users.\n\n`));
+
+  function setActivity() {
+    const status = [`Running on ${client.guilds.size} servers!`, `Used by ${client.users.size} users!`, "Use m!help"];
+    var info = status[Math.floor(Math.random() * status.length)];
+
+    client.user.setActivity(info);
+    console.log(`[Console] Activity set to (${info})`);
+  }
+
+  setInterval(setActivity, 120000);
+};
