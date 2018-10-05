70cdfdca8812da5d5733f824a38e34ab8c41f576
itsYuuki <jakobaindreas11@gmail.com>
2018-10-05 13:56:00 +0800

    Fixed some issues

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,13 @@
 const chalk = require("chalk");
 
-module.exports = client => {
+module.exports = (client) => {
   console.log("Starting up Mei...");
 
   console.log(chalk.green(`Mei is up and running.\nOperating on ${client.guilds.size} servers.\nOperating for ${client.users.size} users.\n\n`));
 
   function setActivity() {
-    const status = [`Running on ${client.guilds.size} servers!`, `Used by ${client.users.size} users!`, "Use m!help"];
-    var info = status[Math.floor(Math.random() * status.length)];
+    let status = [`Running on ${client.guilds.size} servers!`, `Used by ${client.users.size} users!`, "Use m!help"];
+    let info = status[Math.floor(Math.random() * status.length)];
 
     client.user.setActivity(info);
     console.log(`[Console] Activity set to (${info})`);
