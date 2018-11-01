a9f0b072e92e297894683c240c9a3b8d5dbc8175
itsYuuki <jakobaindreas11@gmail.com>
2018-11-01 16:36:46 +0800

    Typo...

--- commands/mod/modlog.js
+++ commands/mod/modlog.js
@@ -17,7 +17,7 @@ module.exports = class ModLogSetCommand extends Commando.Command {
   }
 
   run(message, args) {
-    if (!modRole[message.guild.id]) return message.reply("There are no roles set up for this comamnd to run");
+    if (!modRole[message.guild.id]) return message.reply("There are no roles set up for this command to run");
     
     if (message.member.roles.some(r => modRole[message.guild.id].modroles.includes(r.id)) || message.author.id === message.guild.ownerID) {
       if (args.action.toLowerCase() === "add" || "set") {
