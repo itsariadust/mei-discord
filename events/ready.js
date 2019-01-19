901e0226f43c23ed589c4560bc1b2a1130295d83
itsYuuki <jakobaindreas11@gmail.com>
2019-01-19 19:00:35 +0800

    Added one small thing

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
   console.log(chalk.green(`Mei is up and running.\nOperating on ${client.guilds.size} servers.\nOperating for ${client.users.size} users.\n\n`));
 
   function setActivity() {
-    let status = [`in ${client.guilds.size} servers`, `with ${client.users.size} users`, "m!help for help", "with Kiana", "with a cat"];
+    let status = [`in ${client.guilds.size} servers`, `with ${client.users.size} users`, "m!help for help", "with Kiana", "with Bronya's drills", "with a cat"];
     let info = status[Math.floor(Math.random() * status.length)];
 
     client.user.setActivity(info);
