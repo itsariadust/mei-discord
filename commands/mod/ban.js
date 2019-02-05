dd2fd47d883c93f3f7fe865ddb9c6f6d8ddbc8f0
itsYuuki <jakobaindreas11@gmail.com>
2019-02-05 17:51:46 +0800

    Some changes

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -43,27 +43,35 @@ module.exports = class BanCommand extends Commando.Command {
     if (!member.kickable) return message.reply("**Error:** User can't be banned. Make sure that my highest role is above the user you are trying to kick.");
 
     if (message.member.roles.some(r =>  modRole[message.guild.id].modroles.includes(r.id)) || message.author.id === message.guild.ownerID) {
-      message.guild.ban(member, {
-        days: pruneDays,
-        reason: banMsg
-      }).then(member => {
-        member.send(stripIndents`
-        You have been banned in the server: ${message.guild.name}!
-        "Reason: "${banMsg}"
-        `);
-        message.delete();
+      message.channel.send("Are you sure you want to ban this person? Type `confirm` to execute ban.").then(() => {
+        message.channel.awaitMessages(response => response.content === "confirm", {
+          max: 1,
+          time: 30000,
+          errors: ["time"],
+        })
+          .then(() => {
+            member.send(stripIndents`You have been banned in the server: ${message.guild.name}! "Reason: "${banMsg}"`);
+            message.delete();
+            message.guild.ban(member, {
+              days: pruneDays,
+              reason: banMsg
+            }).then(member => {
+              if (!message.guild.settings.get("modlog")) {
+                return message.say("Done");
+              } else {
+                const embed = new RichEmbed();
+                embed.setTitle("Member Banned");
+                embed.setAuthor(`${member.user.username}#${member.user.discriminator} (${member.user.id})`, member.user.avatarURL);
+                embed.addField("Member banned", `Reason: ${banMsg}`);
+                embed.setTimestamp(message.createdTimestamp);
+                return message.embed(embed);
+              }
+            });
+          })
+          .catch(() => {
+            message.channel.send();
+          });
       });
-
-      if (!message.guild.settings.get("modlog")) {
-        return message.say("Done");
-      } else {
-        const embed = new RichEmbed();
-        embed.setTitle("Member Banned");
-        embed.setAuthor(`${member.user.username}#${member.user.discriminator} (${member.user.id})`, member.user.avatarURL);
-        embed.addField("Member banned", `Reason: ${banMsg}`);
-        embed.setTimestamp(message.createdTimestamp);
-        return message.embed(embed);
-      }
     }
   }
 };
